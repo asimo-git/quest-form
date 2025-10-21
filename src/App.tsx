ede0939a92a8e826c6ea92aec0a80b5ab378d20a
Asimo <rvolenc@gmail.com>
2025-10-21 16:08:30 +0400

    init: install basic dependencies

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Button } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Button variant="subtle" rounded="full">
+          Click me
+        </Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
